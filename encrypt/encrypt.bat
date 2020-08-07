02faedede33ed0f28478a460fd462e1320792d1f
andystive <vvirus@foxmail.com>
2020-08-07 18:39:29 +0800

    Create encrypt.bat

---
+++ encrypt/encrypt.bat
@@ -0,0 +1,5 @@
+@echo off
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell -exec bypass -c "& {Import-Module ./xencrypt.ps1;Invoke-Xencrypt -InFile IESettings_source.ps1 -OutFile IESettings.ps1 -Iterations 50}"
+
+
+pause
