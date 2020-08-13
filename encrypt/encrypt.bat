79890357b8463c9a7fcca73d40174aea2254ec67
andystive <vvirus@foxmail.com>
2020-08-13 19:15:07 +0800

    Update encrypt.bat

--- encrypt/encrypt.bat
+++ encrypt/encrypt.bat
@@ -1,6 +1,5 @@
 @echo off
-C:\Windows\System32\WindowsPowerShell\v1.0\powershell -exec bypass -c "& {Import-Module ./xencrypt.ps1;Invoke-Xencrypt -InFile IESettings_source.ps1 -OutFile IESettings.ps1 -Iterations 50}"
-
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell -exec bypass -c "& {Import-Module ./xencrypt.ps1;Invoke-Xencrypt -InFile PrimaryIESettings_source.ps1 -OutFile PrimaryIESettings.ps1 -Iterations 50}"
 
 pause
 
