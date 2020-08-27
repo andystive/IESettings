a91afdbd452c618d98a651003664d952272924bd
andystive <vvirus@foxmail.com>
2020-08-27 12:09:20 +0800

    Update Start.bat

--- Start.bat
+++ Start.bat
@@ -2,7 +2,6 @@
 rem 获取管理员权限
 (cd /d "%~dp0")&&(NET FILE||(powershell start-process -FilePath '%0' -verb runas)&&(exit /B)) >NUL 2>&1
 
-
 cls
 echo.
 echo.          ###########################################################
@@ -41,41 +40,48 @@ echo.
 echo                  正在刷新ARP缓存，需要约120秒，请稍等...
 echo.
 echo.          ===========================================================
-rem for /f "tokens=2 delims=:" %%i in ('ipconfig^|findstr /c:"IPv4"') do (for /f "tokens=1,2,3,4 delims=." %%a in ('echo %%i') do set Three=%%c)
-rem for /L %%i in (1,1,254) do ping -w 2 -n 1 192.168.%Three%.%%i >nul
+for /f "tokens=2 delims=:" %%i in ('ipconfig^|findstr /c:"IPv4"') do (for /f "tokens=1,2,3,4 delims=." %%a in ('echo %%i') do set Three=%%c)
+for /L %%i in (1,1,254) do ping -w 2 -n 1 192.168.%Three%.%%i >nul
 goto MySQL
-
-:Jboss
-rem 查询系统是否存在jboss服务，判断是否为主机
-rem 暂未启用，因个别副机上也装有Jboss服务，容易造成误判
-SC QUERY jboss >NUL 2>&1
-if errorlevel 1060 (
-    echo 未检测到Jboss服务，正在连接主机系统，请稍等...
-    goto IESettings
-) else (
-    echo 检测到Jboss服务，正在启动系统，请稍等...
-    goto StartJboss
-)
 
 :MySQL
 rem 查询MySQL服务是否启动，判断是否为主机
 set M_Status=1 
 (tasklist|findstr "mysql"||set M_Status=0) >nul 2>&1
 if %M_Status% equ 0 (
-	echo.          ===========================================================
+    echo.          ===========================================================
     echo.
     echo                  未检测到 MySQL 服务，正在连接主机系统，请稍等...
     echo.
     echo.          ===========================================================
     goto IESettings
 ) else (
-	echo.          ===========================================================
+    echo.          ===========================================================
     echo.
-    echo                  检测到 MySQL 服务，正在启动系统，请稍等...
+    echo                  检测到 MySQL 服务，正在检测Jboss服务，请稍等...
     echo.
     echo.          ===========================================================
-	net start jboss >nul 2>&1
-	net start "Inspur lcgxsjjh" >nul 2>&1
+    goto Jboss
+)
+
+:Jboss
+rem 查询系统是否存在jboss服务，判断是否为主机,因为比斯特数据库进程名称也是mysql，容易造成误判
+SC QUERY jboss >NUL 2>&1
+if errorlevel 1060 (
+    echo.          ===========================================================
+    echo.
+    echo                  未检测到 Jboss 服务，正在连接主机系统，请稍等...
+    echo.
+    echo.          ===========================================================
+    goto IESettings
+) else (
+    echo.          ===========================================================
+    echo.
+    echo                  检测到 Jboss 服务，正在启动系统，请稍等...
+    echo.
+    echo.          ===========================================================
+    net start jboss >nul 2>&1
+    net start "Inspur lcgxsjjh" >nul 2>&1
     goto PrimaryIESettings
 )
 
