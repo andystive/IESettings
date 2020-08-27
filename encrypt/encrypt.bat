acd22a7f43d5ac87d58ec9a067a4e8a8a04b4768
andystive <vvirus@foxmail.com>
2020-08-27 15:02:00 +0800

    Delete encrypt.bat

--- encrypt/encrypt.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-C:\Windows\System32\WindowsPowerShell\v1.0\powershell -exec bypass -c "& {Import-Module ./xencrypt.ps1;Invoke-Xencrypt -InFile PrimaryIESettings_source.ps1 -OutFile PrimaryIESettings.ps1 -Iterations 50}"
-
-pause
-
